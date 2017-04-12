73c0fee8a4a62ea1108d7c9a331fa261d1eee1da
Mike Croucher <michael.p.croucher@googlemail.com>
2017-04-12 21:34:46 +0100

    Added downloader to job submission

--- run_photoscan_job.sh
+++ run_photoscan_job.sh
@@ -1,10 +1,13 @@
-#/bin/bash
+#!/bin/bash
 #$ -pe smp 16
 #Prefer the RSE qeueu
 #$ -P rse
 #$ -l h_rt=24:00:00
+module load apps/python/anaconda3-4.2.0
+
 agisoft_dir=~/photoscan/photoscan-pro
-input_dir=/shared/ddcf2/Shared/Group3
+input_dir=$PWD
+python gdrive_download.py 'Photogrammetry Group 4'
 
 (time $agisoft_dir/photoscan.sh -r ./run_agisoft_workflow.py $input_dir) 2>&1 | tee photoscan_output.txt
 
